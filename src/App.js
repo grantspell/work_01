1d75bc1eae0d87b82bf69566794a6358ec56680e
grantspell <spellgrant@gmail.com>
2018-01-31 23:24:13 -0500

    UPDATE

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+// STYLES
+const Wrapper = styled.div`
+  background-color: rgba(155, 25, 35, 0.3);
+  height: 100vh;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  align-content: center;
+  color: rgba(255, 255, 255, .7);
+  font-size: 6vw;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Wrapper className="App">
           <h1>@kaylanspell</h1>
-      </div>
+      </Wrapper>
     );
   }
 }
